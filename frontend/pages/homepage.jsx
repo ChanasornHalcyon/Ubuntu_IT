699a1d4e22b50bd5355425e54eb5dde4af59606d
ChanasornHalcyon <chanasorn@halcyon.co.th>
2025-10-24 10:33:35 +0700

    Edit Error

--- frontend/pages/homepage.jsx
+++ frontend/pages/homepage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useRouter } from "next/router";
 import Navbar from "./components/Navbar";
-import Searchbar from "./components/Searchbar";
 import { motion } from "framer-motion";
 const Homepage = () => {
   const router = useRouter();
